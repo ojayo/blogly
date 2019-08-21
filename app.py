d7a90c9218b442f3c7281be2082a8fcd9674201c
Jay Osborne <jay@Daves-Badios-2.localdomain>
2019-08-21 14:14:44 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+"""Blogly application."""
+
+from flask import Flask, request, render_template, redirect
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "whiskey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def home_page():
+    """ shows home page """
+
+    return render_template('users.html')
+
+    
---
+++ models.py
@@ -0,0 +1,32 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """ Connect to database """
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+	"""" User """
+
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer,
+				   primary_key=True,
+				   autoincrement=True)
+	first_name = db.Column(db.String(50),
+						   nullable=False)
+	last_name = db.Column(db.String(50),
+						   nullable=False)
+	image_url = db.Column(db.String)
+
+	def __repr__ (self):
+		""" show info about user """
+		u = self
+		return f"<User {u.id} {u.first_name} {u.last_name} >"
+
+	
